a0c71b90c3b1ca1b00e11e0feba32e26167a28da
SharmJapay <sharmainejoyjapay.07@gmail.com>
2025-03-24 16:24:07 +0800

    Checkout Authentication fixes. Removed redirecting to /my-account/ page

--- wp-content/themes/sj-demo-theme/functions.php
+++ wp-content/themes/sj-demo-theme/functions.php
@@ -212,39 +212,11 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 
 
 /**
-* Redirecting user to My Account if not logged in during checkout process
- */
-function check_if_logged_in(){
-	$pageid = get_option( 'woocommerce_checkout_page_id' );
-
-	if(!is_user_logged_in() && is_page($pageid)) {
-		$url = add_query_arg(
-			'redirect_to',
-			get_permalink($pageid),
-			site_url('/my-account/') // your my account url
-		);
-		wp_redirect($url);
-		exit;
-	}
-
-	if(is_user_logged_in())	{
-		if(is_page(get_option( 'woocommerce_myaccount_page_id' ))) {
-			$redirect = $_GET['redirect_to'];
-			if (isset($redirect)) {
-				echo '<script>window.location.href = "'.$redirect.'";</script>';
-			}
-		}
-	}
-}
-add_action('template_redirect','check_if_logged_in');
-
-
-/**
  * Implementing user authentication pop-up if user is not logged-in during checkout
  */
 
 function activate_authentication_popup() {
-    if ( !is_user_logged_in()) {
+    if ( is_checkout() && !is_user_logged_in()) {
         require get_template_directory() . '/woocommerce/checkout/auth-popup.php';
     }
 
